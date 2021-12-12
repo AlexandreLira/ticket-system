87eed4176eb16b06a17eb548adc64fb7c8dfc38d
Alexandre Lira <alexandrelira112@gmail.com>
2021-12-12 13:15:25 -0300

    upload image to firebase storage and change the name of user

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -115,6 +115,8 @@ function AuthProvider({ children }){
                 register,
                 login,
                 logout,
+                storageUser,
+                setUser
             }}
         >
             {children}
--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -1,4 +1,11 @@
 import { useState, useContext } from 'react'
+import { db, storage } from '../../services/firebaseConnection'
+import {
+    doc,
+    serverTimestamp,
+    setDoc
+} from 'firebase/firestore'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 
 import Title from '../../components/Title/title'
 import Header from '../../components/Header/header'
@@ -6,15 +13,97 @@ import './profile.css'
 
 import { FiSettings, FiUpload } from 'react-icons/fi'
 import avatar from "../../assets/avatar.png"
-
+import { toast } from 'react-toastify'
 import { AuthContext } from "../../contexts/auth"
 
 function Profile(){
-    const { user, logout } = useContext(AuthContext)
+    const { user, logout, setUser, storageUser } = useContext(AuthContext)
 
     const [ name,      setName      ] = useState(user?.name)
-    const [ email,     setEmail     ] = useState(user?.email)
     const [ avatarUrl, setAvatarUrl ] = useState(user?.avatarUrl)
+    const [ newAvatarUrl, setNewAvatarUrl ] = useState(user?.avatarUrl)
+    const [progress, setProgress] = useState(0)
+    const email =  user?.email
+    const userRef = doc(db, "users", user.id)
+
+    async function handleSave(event){
+        event.preventDefault()
+        
+        if(name !== user.name && newAvatarUrl === user.avatarUrl) {
+            await setDoc(userRef, {
+                name: name, 
+                updateAt: serverTimestamp()
+            }, 
+            {
+                merge: true
+            })
+            .then( value => {
+                let data = {
+                    ...user,
+                    name: name
+                }
+                setUser(data)
+                storageUser(data)
+                toast.success('Perfil editado com sucesso!')
+            }).catch( error => {
+                toast.warning('Ops! algo deu errado')
+            })
+        }
+        else if(newAvatarUrl !== user.avatarUrl && name !== user.name){
+            handleUpload(newAvatarUrl)
+        }
+
+    }
+
+    function handleFile(event){
+        const image = event.target.files[0]
+        console.log(image)
+        if(image){
+           if(image.type === 'image/jpeg' || image.type === 'image/png'){
+               setNewAvatarUrl(image)
+               setAvatarUrl(URL.createObjectURL(image))
+               console.log(URL.createObjectURL(image))
+           }
+        }
+    }
+
+    async function handleUpload(image){
+        if(!image) return
+        const storageRef = ref(storage, `images/${user.id}/${newAvatarUrl.name}`)
+        const uploadTask = uploadBytesResumable(storageRef, newAvatarUrl)
+        uploadTask.on("state_changed", snapshot => {
+            const prog = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+            setProgress(prog)
+        },
+        () => {
+            toast.error('errro')
+            setProgress(0)
+        },
+        () => {
+            toast.success('sucess')
+            setProgress(0)
+            getDownloadURL(uploadTask.snapshot.ref)
+            .then(async url => {
+                await setDoc(userRef, {
+                    avatarUrl: url,
+                    name: name,
+                    updateAt: serverTimestamp()
+                }, 
+                {merge: true})
+                .then(() => {
+                    let data = {
+                        ...user,
+                        name: name,
+                        avatarUrl: url
+                    }
+                    setUser(data)
+                    storageUser(data)
+                })
+            })
+        })
+
+    }
+
     return(
         <div>
             <Header/>
@@ -24,13 +113,17 @@ function Profile(){
                 </Title>
 
                 <div className="container">
-                    <form className="form-profile">
+                    <form className="form-profile" onSubmit={handleSave}>
                         <label className="label-avatar">
                             <span>
                                 <FiUpload color="#FFF" size={25}/>
                             </span>
 
-                            <input type="file" accept="image/*" />
+                            <input 
+                                type="file" 
+                                accept="image/*"
+                                onChange={handleFile}    
+                            />
                             <img 
                                 src={avatarUrl === null ? avatar : avatarUrl} 
                                 width="250" 
@@ -53,7 +146,7 @@ function Profile(){
                             disabled={true}
                         />
 
-                        <button>Salvar</button>
+                        <button>{progress > 1 ? `Salvando ${progress}%` : 'Salvar' }</button>
 
                     </form>
                 </div>
